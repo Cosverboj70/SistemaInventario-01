f3a3ee8acbf5090df3207111880715d5f046a558
Cosverboj70 <cverduzcob.cln1@sin.conalep.edu.mx>
2023-12-13 13:16:40 -0700

    CRUD Bodegas

---
+++ Sistema Inventario/Areas/Admin/Controllers/CoordinacionController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+using SistemaInventario.Utilidades;
+
+namespace Sistema_Inventario.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoordinacionController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public CoordinacionController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region API
+        [HttpGet]
+        public async Task<ActionResult> Obtenertodos()
+        {
+            var todos = await _unidadTrabajo.Coordinacion.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+        #endregion
+
+        //public async Task<IActionResult> Upsert(int? id)
+        //{
+        //    Bodega bodega = new Bodega();
+
+        //    if (id == null)
+        //    {
+        //        // Crear una nueva Bodega
+        //        bodega.Estado = true;
+        //        return View(bodega);
+        //    }
+        //    // Actualizamos Bodega
+        //    bodega = await _unidadTrabajo.Bodega.Obtener(id.GetValueOrDefault());
+        //    if (bodega == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(bodega);
+        //}
+
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Upsert(Bodega bodega)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        if (bodega.Id == 0)
+        //        {
+        //            await _unidadTrabajo.Bodega.Agregar(bodega);
+        //            TempData[DS.Exitosa] = "Bodega creada Exitosamente";
+        //        }
+        //        else
+        //        {
+        //            _unidadTrabajo.Bodega.Actualizar(bodega);
+        //            TempData[DS.Exitosa] = "Bodega actualizada Exitosamente";
+        //        }
+        //        await _unidadTrabajo.Guardar();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    TempData[DS.Error] = "Error al grabar Bodega";
+        //    return View(bodega);
+        //}
+
+
+    }
+}
--- SistemaInventario.Utilidades/DS.cs
+++ SistemaInventario.Utilidades/DS.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace SistemaInventario.Utilidades
 {
-	public static class DS
-	{
-	}
+    public static class DS
+    {
+        public const string Exitosa = "Exitosa";
+        public const string Error = "Error";
+
+        public const string ImagenRuta = @"\imagenes\producto\";
+        public const string ssCarroCompras = "Sesion carro Compras";
+
+        public const string Role_Admin = "Admin";
+        public const string Role_Cliente = "Cliente";
+        public const string Role_Inventario = "Inventario";
+
+        // Estados de la Orden
+        public const string EstadoPendiente = "Pendiente";
+        public const string EstadoAprobado = "Aprobado";
+        public const string EstadoEnProceso = "Procesando";
+        public const string EstadoEnviado = "Enviado";
+        public const string EstadoCancelado = "Cancelado";
+        public const string EstadoDevuelto = "Devuelto";
+        // Estado del Pago de la Orden
+        public const string PagoEstadoPendiente = "Pendiente";
+        public const string PagoEstadoAprobado = "Aprobado";
+        public const string PagoEstadoRetrasado = "Retrasado";
+        public const string PagoEstadoRechazado = "Rechazado";
+    }
 }
